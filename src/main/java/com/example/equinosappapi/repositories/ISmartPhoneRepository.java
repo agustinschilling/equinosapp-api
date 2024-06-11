88355d81f3157e8b0f0fc4fac41427248f432f6f
Agustin Schilling <schillingagustin@gmail.com>
2024-06-11 15:55:54 -0300

    Remover ISmartPhoneRepository.java

--- src/main/java/com/example/equinosappapi/repositories/ISmartPhoneRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.equinosappapi.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ISmartPhoneRepository extends JpaRepository<SmartPhone, Long> {
-}
