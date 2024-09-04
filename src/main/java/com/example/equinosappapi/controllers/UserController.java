cbd1abb503ed7951bdb85a2353e369a7a5fb9116
Agustin Schilling <schillingagustin@gmail.com>
2024-09-03 22:55:58 -0300

    Agrego endpoint para establecer una imagen de perfil de usuario

--- src/main/java/com/example/equinosappapi/controllers/UserController.java
+++ src/main/java/com/example/equinosappapi/controllers/UserController.java
@@ -1,14 +1,22 @@
 package com.example.equinosappapi.controllers;
 
 import com.example.equinosappapi.models.User;
+import com.example.equinosappapi.services.ImageService;
 import com.example.equinosappapi.services.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Optional;
+
+import static com.example.equinosappapi.security.JwtGenerator.getUsernameFromJwt;
+import static com.example.equinosappapi.services.ImageService.getImageExtension;
 
 @RestController
 @RequestMapping("/api/users")
@@ -16,9 +24,12 @@ public class UserController {
 
     private final UserService userService;
 
+    private final ImageService imageService;
+
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, ImageService imageService) {
         this.userService = userService;
+        this.imageService = imageService;
     }
 
     @Operation(summary = "Obtener usuario por identificacion")
@@ -59,9 +70,35 @@ public class UserController {
         Optional<User> user = userService.readOne(id);
         if (user.isPresent()) {
             userService.delete(id);
+            String imagePath = user.get().getImage();
+            if (imagePath != null) {
+                imageService.deleteImage("users" + File.separator + imagePath);
+            }
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
+    }
+
+    @Operation(summary = "Establecer imagen perfil de usuario")
+    @PostMapping("/image")
+    public ResponseEntity<Void> uploadProfileImage(@RequestPart("image") MultipartFile image, HttpServletRequest request) throws IOException {
+        String token = UserService.extractTokenFromRequest(request);
+        String username = getUsernameFromJwt(token);
+        User user = userService.getByUsername(username);
+
+        String userImage = user.getImage();
+        if (userImage != null && !userImage.isEmpty()) {
+            imageService.deleteImage("/users/" + userImage);
+        }
+
+        String originalFileName = image.getOriginalFilename();
+        String extension = getImageExtension(originalFileName);
+        byte[] bytes = image.getBytes();
+        String imageName = imageService.saveImage(bytes, extension, "users");
+        user.setImage(imageName);
+        userService.update(user);
+
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
--- src/main/java/com/example/equinosappapi/models/User.java
+++ src/main/java/com/example/equinosappapi/models/User.java
@@ -19,6 +19,7 @@ public class User {
     private String username;
     private String email;
     private String password;
+    private String image;
 
     @Enumerated(EnumType.STRING)
     private Role role = Role.USER;
