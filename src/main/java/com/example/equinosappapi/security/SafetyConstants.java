2cae9090597c0fb78ac9b57755129f7d23e855a5
nicobastien <nicobastien927@gmail.com>
2024-11-29 11:09:20 -0300

    Aumento límite de sesión a 10min.

--- src/main/java/com/example/equinosappapi/security/SafetyConstants.java
+++ src/main/java/com/example/equinosappapi/security/SafetyConstants.java
@@ -1,6 +1,6 @@
 package com.example.equinosappapi.security;
 
 public class SafetyConstants {
-    public static final long JWT_EXPIRATION_TOKEN = 300000; // 5 min
+    public static final long JWT_EXPIRATION_TOKEN = 6000000; // 5 min
     public static final String JWT_SIGNATURE = "equinoapp";
 }
